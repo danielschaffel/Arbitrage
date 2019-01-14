9f5d754e65eb8f32c68776e49e85830c410a5021
danielschaffel <deschaffel97@gmail.com>
2019-01-14 17:24:36 -0500

    found the info on x-rates trying to figure out how to extract it

--- Scrapers/scrapetest.py
+++ Scrapers/scrapetest.py
@@ -3,7 +3,7 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen("https://www.x-rates.com/table/")
+html = urlopen("https://www.x-rates.com/")
 bsobj = BeautifulSoup(html,"html.parser")
-for link in bsobj.findAll("a", href=re.compile("(/table/)")):
-    print(link)
+for link in bsobj.findAll("a", href=re.compile("/graph/")):
+    print(re.findAll("\w" ,link)
