849972cbdfeeaa301814103379f9aed0c19fa92a
danielschaffel <deschaffel97@gmail.com>
2019-01-22 15:31:05 -0500

    creating edges works... However the Digraph needs to change from taking integers to strings. Going to try making a map with string as key and adj array as values

--- Scrapers/scrapetest.py
+++ Scrapers/scrapetest.py
@@ -11,15 +11,21 @@ def getRates():
     html = urlopen("https://www.x-rates.com/table/?from=USD&amount=1")
     bsobj = BeautifulSoup(html,"html.parser")
     #so the link is a BeutifulSoup object so if i want to access just remember that
-    links =  bsobj.findAll("tr") 
+    links =  bsobj.findAll("tr")
+    graph = EdgeWeightedDigraph.Digraph(len(links))
     for link in links:
         #findAll returns a set with all of the info so that is how i need to be accesing it
-        val = link.findAll("a")
         name = link.findAll("td")
         for nam in name:
-            print(name.pop().get_text())
-            print(name.pop().get_text())
-            print(name.pop().get_text())
+            fromCurr = name.pop().get_text()
+            toCurr = name.pop().get_text()
+            currName = name.pop().get_text()
+            edgeTo = EdgeWeightedDigraph.DirectedEdge("USD" , currName , toCurr)
+            edgeFrom = EdgeWeightedDigraph.DirectedEdge(currName , "USD" , fromCurr)
+            print(str(edgeTo.edge_from()) + "---->" + str(edgeTo.get_weight()) + "---->" + str(edgeTo.edge_to()))
+            print(str(edgeFrom.edge_from()) + "---->" + str(edgeFrom.get_weight()) + "---->" + str(edgeFrom.edge_to()))
+            #graph.add_edge(edgeTo)
+            # graph.add_edge(edgeFrom)
         print()
 
 getRates()
